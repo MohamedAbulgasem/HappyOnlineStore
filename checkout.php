ad06622b6a073777dbde0dd926057443955f0bc8
Net29 <algiriany93@gmail.com>
2017-09-29 09:49:53 +0200

    Code Indented

--- checkout.php
+++ checkout.php
@@ -18,16 +18,16 @@
 
 			if($queryResult){
                 $row = $queryResult->fetch_array();
-				echo '<table style="width:80%; height: 60px; border: 2px solid #632F8D; margin: 10px auto; margin-bottom: 10px;">';
-				echo '<tr>';
-				echo '<td style="border-right: 2px solid #632F8D; height:100%; width: 5%; text-align:center;"><b>' . $counter . '</b></td>';
-				echo '<td style="height:100%; width: 35%; text-align:center;"><b>' . htmlentities($row['item_name']) . '</b></td>';
-				echo '<td style="border-right: 2px solid #632F8D; height:100%; width: 10%; text-align:center;"><img src=" ' . htmlentities($row['image_link']) . ' " alt=" ' . htmlentities($row['item_name']) . ' " width="50px" height="50px"/></td>';
-				echo '<td style="border-right: 2px solid #632F8D; height:100%; width: 15%; text-align:center;"><b>Price</b><br />' . htmlentities($row['price']) . '</td>';
-				echo '<td style="border-right: 2px solid #632F8D; height:100%; width: 10%; text-align:center;"><b>Quantity</b><br />' . $vals . '</td>';
-				echo '<td style="height:100%; width: 25%; text-align:center;"><b>Subtotal</b><br />R ' . ($vals * $row['price']) . '</td>';
-				echo '</tr>';
-				echo '</table>';	
+								echo '<table style="width:80%; height: 60px; border: 2px solid #632F8D; margin: 10px auto; margin-bottom: 10px;">';
+								echo '<tr>';
+								echo '<td style="border-right: 2px solid #632F8D; height:100%; width: 5%; text-align:center;"><b>' . $counter . '</b></td>';
+								echo '<td style="height:100%; width: 35%; text-align:center;"><b>' . htmlentities($row['item_name']) . '</b></td>';
+								echo '<td style="border-right: 2px solid #632F8D; height:100%; width: 10%; text-align:center;"><img src=" ' . htmlentities($row['image_link']) . ' " alt=" ' . htmlentities($row['item_name']) . ' " width="50px" height="50px"/></td>';
+								echo '<td style="border-right: 2px solid #632F8D; height:100%; width: 15%; text-align:center;"><b>Price</b><br />' . htmlentities($row['price']) . '</td>';
+								echo '<td style="border-right: 2px solid #632F8D; height:100%; width: 10%; text-align:center;"><b>Quantity</b><br />' . $vals . '</td>';
+								echo '<td style="height:100%; width: 25%; text-align:center;"><b>Subtotal</b><br />R ' . ($vals * $row['price']) . '</td>';
+								echo '</tr>';
+								echo '</table>';
 			}
 			$counter++;
 		}
